7b2e2e5e96a220860691ca54bb783f8039c991e7
Yahor Dzemiashkevich <yahor_dzemiashkevich@MacBook-Pro-Yahor.local>
2018-09-08 16:56:11 +0300

    Add runtime component - JParser

---
+++ jparser-runtime/src/main/java/com.dzemiashkevich.parser/JParserApplication.java
@@ -0,0 +1,13 @@
+package com.dzemiashkevich.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JParserApplication.class, args);
+    }
+
+}
