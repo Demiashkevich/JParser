a72894a4ddf4da09907b67bb4b27fe99cff91c79
Yahor Dzemiashkevich <yahor_dzemiashkevich@MacBook-Pro-Yahor.local>
2018-09-10 21:47:11 +0300

    Add common api to source/output using

---
+++ jparser-api/src/main/java/com.dzemiashkevich.parser/Resource.java
@@ -0,0 +1,39 @@
+package com.dzemiashkevich.parser;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Resource {
+
+    private String name;
+    private Map<String, String> params = new HashMap<String, String>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, String> params) {
+        this.params = params;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return EqualsBuilder.reflectionEquals(this, object);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+}
